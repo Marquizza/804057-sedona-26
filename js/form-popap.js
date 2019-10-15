db506d5be04d8d28d5e14fb1459dec5ad1110094
Kate <katikla2@mail.ru>
2019-10-15 19:14:11 +0300

    Личный проект - исправления 15,10

---
+++ js/form-popap.js
@@ -0,0 +1,7 @@
+var searchBtn = document.querySelector(".hotel-search");
+var	popup = document.querySelector(".data-order");
+
+searchBtn.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	popup.classList.toggle("data-order");
+});
