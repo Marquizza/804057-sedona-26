4eb76fed37975ced460eae39b06f843476a2f2bb
Kate <katikla2@mail.ru>
2019-10-21 20:19:07 +0300

    Подготовка в защите

--- js/form-popap.js
+++ js/form-popap.js
@@ -3,5 +3,5 @@ var	popup = document.querySelector(".data-order");
 
 searchBtn.addEventListener("click", function(evt) {
 	evt.preventDefault();
-	popup.classList.toggle("data-order");
+	popup.classList.toggle("data-order-active");
 });
